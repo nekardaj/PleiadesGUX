e65367b4838906a99b149acf3012dbcd8f228e08
Jan Nekarda <honzanekarda@centrum.cz>
2023-12-03 20:07:02 +0100

    Game start log.

--- Assets/Scripts/IntroButton.cs
+++ Assets/Scripts/IntroButton.cs
@@ -9,6 +9,7 @@ public class IntroButton : MonoBehaviour {
     public void Play()
     {
         manager.FadeToLevel(1);
+        Debug.Log($"[log] Started game with version {ParametersManager.Instance.GetParameterSettings()}");
         //Time.timeScale = 1f;
         //SceneManager.LoadScene("MovementScene");
         //Debug.Log("Play");
